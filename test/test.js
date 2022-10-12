3ddd2bf58e228b8f7e674704d4ab69278686f60c
echeverriaalex <aleex.naahuel@outlook.com>
2022-10-12 17:34:57 -0300

    sigo agregando testing

--- test/test.js
+++ test/test.js
@@ -4,6 +4,7 @@ const {assert} = require('chai');
 const db = require('../models/index');
 
 
+
 /*
 const sinon = require('sinon');
 var chai = require('chai');
@@ -11,13 +12,94 @@ var chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 */
 
-describe.only('GET /users', ()=>{
+beforeEach(() => {
+    db.sequelize.truncate({ cascade: true })
+});
 
-    it('check status 200', (done)=>{
+after(() => {
+    server.close();
+})
+
+describe.only('GET /users', ()=>{
+    it('Get all users and check status 200', (done)=>{
         request(app)
         .get('/users')
         .expect('Content-Type', /json/)
         .expect(200)
         .end(done);
 	});
+    
+    it("check theres only one user", done => {
+        request(app)
+        .post('/user')
+        .send({
+            "firstname": "Alex",
+            "lastname": "Echeverria",
+            "username": "Alexiñho",
+            "email": "alex@echeverria.com",
+            "password": "contraseñadealex"
+        })
+        .set('Accept', 'application/json')
+        .then(user => {
+            request(app)
+            .get('/users')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .then(res => {
+                assert.lengthOf(res.body, 1);
+                done();
+            })
+        }).catch(err => done());
+    })
 });
+
+describe.only("POST /user", () => {
+    it("Create an user and check status 201", done => {
+        request(app)
+        .post('/user')
+        .send({
+            "firstname": "Alex",
+            "lastname": "Echeverria",
+            "username": "Alexiñho",
+            "email": "alex@echeverria.com",
+            "password": "contraseñadealex"
+        })
+        .set('Accept', 'application/json')
+        .expect(201)
+        .end(done);
+    })
+});
+
+
+describe.only("GET /cars", () => {
+    beforeEach(async () => {
+        let user = db.User.build({
+            "id": 3,
+            "firstname": "Alex",
+            "lastname": "Echeverria",
+            "username": "Alexiñho",
+            "email": "alex@echeverria.com",
+            "password": "contraseñadealex",
+            "role": "user"
+        })
+        await user.save();
+        await db.car.create({
+            "brand": "Mitsubishi",
+            "model": "Evo 9",
+            "year": 2008,
+            "color": "black",
+            "UserId": user.id
+        });
+    })
+
+    it("will test cars", done => {
+        request(app)
+        .get('/cars')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .then(res => {
+            assert.lengthOf(res.body, 1);
+            done();
+        });
+    })
+})
